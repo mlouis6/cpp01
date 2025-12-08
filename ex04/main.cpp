32af29367d0fc7c8424f77d8505bf0a1b1213f43
ML <louism0495@gmail.com>
2025-12-08 13:02:21 +0100

    sed still need fixing

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 09:55:35 by mlouis            #+#    #+#             */
-/*   Updated: 2025/12/05 13:10:18 by mlouis           ###   ########.fr       */
+/*   Updated: 2025/12/08 13:01:57 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,52 +49,41 @@ int	main(int argc, char *argv[])
 	std::string	line;
 	while (std::getline(ifile, line))
 	{
-		/**
-		 * bienvenue
-		 * 'nv' -> 'hello'
-		 * 1) 'bie' (from [0, 2])
-		 * 2) 'enue' (from [len -5, len -1])
-		 * 3) 'hello' (from [pos, args[3].len + pos])
-		 * 
-		 * find in while in case of multiple (like 'en' in bienvenue)
-		 * but find from last pos
-		 * avoid 'en' -> 'english' to inifinite loop
-		 * 
-		 */
+		size_t pos = 0;
+		size_t	count = 0;
 
-		if (int pos = line.find(argv[2]) != std::string::npos)
+		while ((pos = line.find(argv[2], pos)) != std::string::npos) 
 		{
-			int	nl_len = line.length() + args[3].length() - args[2].length();
-			std::string	new_line(nl_len, '\0');
-			for (int i = 0; i < (int) line.length(); ++i)
-			{
-				// std::cout << "i= " << i << "; pos= " << pos << "; len= " << args[2].length() << "\n";
+			pos += args[2].length();
+			++count;
+		}
+		pos = 0;
+		size_t	nl_len = line.length() + args[3].length() * count - args[2].length() * count;
+		std::string	new_line(nl_len + 1, '\0');
+		std::string	tmp_line = line;
+		while ((pos = tmp_line.find(argv[2], pos)) != std::string::npos) 
+		{
+			std::cout << "POS= " << pos << "\n";
+			for (size_t i = 0 ; i < pos ; ++i)
 				new_line[i] = line[i];
-				// TODO: offset
-				int j = 0;
-				for (; i >= pos - 1 && i < (int) args[2].length(); ++j)
-				{
-					new_line[i] = args[3][j];
-					// std::cout << "\n(" << i << ")nl= '" << new_line[i] << "'\n";
-					// std::cout << j << ")" << args[3][j] << " ";
-					++i;
-					// if (i > (int) line.length())
-					// 	break ;
-				}
-				std::string tmp(nl_len, '\0'); //TODO: find precise length
-				while (j < nl_len)
-				{
-					
-					new_line
-				}
-				std::cout << "nl= '" << new_line << "'\n";
+			for (size_t i = 1 ; i <= line.length() - pos - args[2].length(); ++i)
+				new_line[nl_len - i] = line[line.length() - i];
+			// TODO: fix issue
+			size_t	pos2 = line.find(argv[2], pos);
+			for (size_t i = 0 ; i < args[3].length() ; ++i)
+			{
+				new_line[pos2 + i] = args[3][i];
+				std::cout << "pos: " << pos2 << " ; i= " << i << "\n";
 			}
-			ofile << new_line + "\n";
+			//? end issue
+			pos += args[2].length();
+			line = new_line;
 		}
-		else
-		{
-			ofile << line + "\n";
-		}
+		std::cout << "\n========\n";
+		for (size_t i = 0 ; i < nl_len ; ++i)
+			std::cout << new_line[i] << "(" << i << ")";
+		std::cout << "\n========\n";
+		ofile << line + "\n";
 	}
 	ifile.close();
 	ofile.close();
