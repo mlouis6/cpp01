7c88593dd16e9442de9a54ce9bfd98231fc69ccd
ML <louism0495@gmail.com>
2025-12-09 11:42:21 +0100

    segfault

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 09:55:35 by mlouis            #+#    #+#             */
-/*   Updated: 2025/12/08 13:01:57 by mlouis           ###   ########.fr       */
+/*   Updated: 2025/12/09 11:42:00 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,37 +52,44 @@ int	main(int argc, char *argv[])
 		size_t pos = 0;
 		size_t	count = 0;
 
+		if (line.find(argv[2], pos) == std::string::npos)
+		{
+			ofile << line + "\n";
+			continue ;
+		}
 		while ((pos = line.find(argv[2], pos)) != std::string::npos) 
 		{
 			pos += args[2].length();
 			++count;
 		}
 		pos = 0;
-		size_t	nl_len = line.length() + args[3].length() * count - args[2].length() * count;
+		size_t	nl_len = line.length() - 1 + (args[3].length()) * count - (args[2].length()) * count;
+		std::cout << "LENNL = " << nl_len << "(linelen= " << args[3].length() << ")" << std::endl;
 		std::string	new_line(nl_len + 1, '\0');
 		std::string	tmp_line = line;
 		while ((pos = tmp_line.find(argv[2], pos)) != std::string::npos) 
 		{
-			std::cout << "POS= " << pos << "\n";
 			for (size_t i = 0 ; i < pos ; ++i)
 				new_line[i] = line[i];
-			for (size_t i = 1 ; i <= line.length() - pos - args[2].length(); ++i)
-				new_line[nl_len - i] = line[line.length() - i];
-			// TODO: fix issue
+			size_t	j = 0;
+			while (new_line[j])
+				++j;
+			for (size_t i = 0 ; i < tmp_line.length() - pos - args[2].length() ; ++i)
+			{
+				new_line[args[3].length() + j + i] = tmp_line[pos + args[2].length() + i];
+				// std::cout << 
+			}
 			size_t	pos2 = line.find(argv[2], pos);
 			for (size_t i = 0 ; i < args[3].length() ; ++i)
-			{
 				new_line[pos2 + i] = args[3][i];
-				std::cout << "pos: " << pos2 << " ; i= " << i << "\n";
+			for (size_t i = 0 ; i < nl_len ; ++i)
+			{
+				std::cout << "(" << i << ")" << new_line[i];
 			}
-			//? end issue
+			std::cout << std::endl;
 			pos += args[2].length();
 			line = new_line;
 		}
-		std::cout << "\n========\n";
-		for (size_t i = 0 ; i < nl_len ; ++i)
-			std::cout << new_line[i] << "(" << i << ")";
-		std::cout << "\n========\n";
 		ofile << line + "\n";
 	}
 	ifile.close();
