ae497e2fe05f751b7a64ae3a25870266dc816aa4
ML <louism0495@gmail.com>
2025-12-18 17:56:39 +0100

    fix harl struct lvl

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,17 +6,33 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 09:17:19 by mlouis            #+#    #+#             */
-/*   Updated: 2025/12/17 11:13:05 by mlouis           ###   ########.fr       */
+/*   Updated: 2025/12/18 17:56:02 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 #include <iostream>
-#include <string>
+
+Harl::s_lvl	Harl::_lvl[] = {
+								{ "DEBUG", &Harl::debug },
+								{ "INFO", &Harl::info },
+								{ "WARNING", &Harl::warning },
+								{ "ERROR", &Harl::error }
+							};
 
 void	Harl::complain(std::string level)
 {
-	;
+	int size = sizeof(_lvl) / sizeof(struct s_lvl);
+
+	for (int i = 0 ; i < size ; ++i)
+	{
+		if (level.compare(_lvl[i].name) == 0)
+		{
+			_lvl[i].func();
+			return ;
+		}
+	}
+	std::cout << "Error\nInvalid complain\n";
 }
 
 void	Harl::debug(void)
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,38 +6,30 @@
 /*   By: mlouis <mlouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 09:17:27 by mlouis            #+#    #+#             */
-/*   Updated: 2025/12/17 13:07:08 by mlouis           ###   ########.fr       */
+/*   Updated: 2025/12/18 17:46:19 by mlouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
 
-enum	e_lvl
-{
-	DEBUG,
-	INFO,
-	WARNING,
-	ERROR
-};
-
-
+# include <string>
 
 class Harl
 {
 	public:
 		void	complain(std::string level);
+	private:
+		static void	debug(void);
+		static void	info(void);
+		static void	warning(void);
+		static void	error(void);
 		struct	s_lvl
 		{
-			std::string	level;
-			int*		action();
+			std::string	name;
+			void		(*func)();
 		};
-	private:
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
+		static struct s_lvl	_lvl[];
 };
-
 
 #endif
